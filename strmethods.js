3d375ae36577e408ac3dcc5032f1728b2ed65a23
mdafsha9 <mdafsha9@gmail.com>
2021-11-17 00:48:42 -0600

    trim() method

---
+++ strmethods.js
@@ -0,0 +1,11 @@
+//trim()
+//toUpperCase()
+//toLowerCase()
+//slice()
+let fruitName = "      Apple       ";
+console.log(fruitName.length);
+console.log(fruitName);
+
+let newString = fruitName.trim();
+console.log(newString.length);
+console.log(newString);
