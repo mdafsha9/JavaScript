dc9c65a049585bbeb35666e3a7f3426781e04a34
mdafsha9 <mdafsha9@gmail.com>
2021-11-17 00:37:06 -0600

    about string indexing

---
+++ strindex.js
@@ -0,0 +1,15 @@
+//string indexing
+
+let flowerName = "sunflower";
+
+// s u n f l o w e r
+// 0 1 2 3 4 5 6 7 8
+
+console.log(flowerName[8]);
+
+//length of string
+console.log(flowerName.length);
+
+//last index of string: length-1
+flowerName = "sunflowerhvfngjdfgkj";
+console.log(flowerName[flowerName.length-1]);
