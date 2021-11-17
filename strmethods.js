030a80a758adf9ab09c968296edaba5b5a3735fe
mdafsha9 <mdafsha9@gmail.com>
2021-11-17 00:52:59 -0600

    uppercase & lowercase methods

--- strmethods.js
+++ strmethods.js
@@ -2,10 +2,20 @@
 //toUpperCase()
 //toLowerCase()
 //slice()
+
+//trim() method is used to remove space,
 let fruitName = "      Apple       ";
 console.log(fruitName.length);
 console.log(fruitName);
-
+//here we have to take another new string to assign a trim()
 let newString = fruitName.trim();
 console.log(newString.length);
 console.log(newString);
+
+
+//toUpperCase
+console.log(newString.toUpperCase());
+
+//toLowerCase
+newString = "APPLE";
+console.log(newString.toLowerCase());
