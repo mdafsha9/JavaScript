f831f3fde9e1f407d214188ef95222a379571ca4
mdafsha9 <mdafsha9@gmail.com>
2021-11-17 04:18:45 -0600

    if else condition

---
+++ TorFVal.js
@@ -0,0 +1,15 @@
+//Truthy or falsy values
+
+//if else condition
+let age = 17;
+if(age>=18){
+  console.log("user can play cricket");
+}else{
+  console.log("user can play Mario");
+}
+//Below  are falsy values
+// false
+// " "
+// undefined
+// null
+// 0
