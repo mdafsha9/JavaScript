27591b6f8dfcf8363460be1a42df62e1be5a4c97
mdafsha9 <mdafsha9@gmail.com>
2021-11-17 04:04:03 -0600

    boolean and comparison operator

---
+++ boolcomp.js
@@ -0,0 +1,15 @@
+//booleans & comaprison operators
+
+let num1 = 9;
+let num2 = 8;
+//it returns true
+console.log(num1>num2);
+//here it returns false
+console.log(num1<num2);
+
+num1 = "9";
+num2 = 9;
+//here it returns true because it check only value
+console.log(num1 == num2);
+//here it returns false because it checks both value and typeof
+console.log(num1 === num2);
