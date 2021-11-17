6e61469f2462b6c57b883545e3cf0f5e2262a163
mdafsha9 <mdafsha9@gmail.com>
2021-11-17 05:21:09 -0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,16 @@
+//Ternary operator or conditional operator
+
+let age = 5;
+let drink;
+if(age>=5){
+  drink = "coffee";
+}else{
+  drink = "milk";
+}
+console.log(drink);
+
+//using ternary operator we can reduce the code
+let years = 3;
+
+let softDrink = years>=5 ? "coffee" : "milk";
+console.log(softDrink);
