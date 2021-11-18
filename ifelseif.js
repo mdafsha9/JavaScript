ac3c740f7de6a15a9855c67d508f66fe9059b8eb
mdafsha9 <mdafsha9@gmail.com>
2021-11-18 01:18:06 -0600

    ifelseif consition

---
+++ ifelseif.js
@@ -0,0 +1,19 @@
+//if
+//else if
+//else if
+//else if
+//else
+let tempInDegree = 20;
+if(tempInDegree<0){
+  console.log("Falling snow outside!");
+}else if(tempInDegree<15){
+  console.log("It is cold outside!");
+}else if(tempInDegree<25){
+  console.log("Wheather is okay now!");
+}else if(tempInDegree<35){
+  console.log("Feeling like swimm!");
+}else if(tempInDegree<45){
+  console.log("Turn on AC!");
+}else{
+  console.log("Too hot outside!")
+}
