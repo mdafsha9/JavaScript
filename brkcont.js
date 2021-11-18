3a06960b050f9251ed7ea8fe23163cb22779e2d4
mdafsha9 <mdafsha9@gmail.com>
2021-11-18 03:53:09 -0600

    continue keyword

--- brkcont.js
+++ brkcont.js
@@ -1,9 +1,17 @@
-//break keywords
-//continue keywords
+//break keywords---it break the execution where we used break keyword
+
 
 for(var i=1; i<=10; i++){
   if(i === 4){
     break;
+  }
+  console.log(i);
+}
+
+//continue keywords--continue skip the number where we used continue keyword
+for(var i=1; i<=10; i++){
+  if(i === 4){
+    continue;
   }
   console.log(i);
 }
