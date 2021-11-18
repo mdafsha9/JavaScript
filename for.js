4e50c10b885a5eedc5a2cf74130f912116528763
mdafsha9 <mdafsha9@gmail.com>
2021-11-18 03:33:44 -0600

    print sum of 1 to 10 values

--- for.js
+++ for.js
@@ -4,3 +4,10 @@
 for(var i = 0; i<=10; i++){
   console.log(i);
 }
+
+let total = 0;
+let num = 10;
+for(let i = 1; i<=10; i++){
+  total =total+i;
+}
+console.log("Print sum of 1 to 10 values " +total);
