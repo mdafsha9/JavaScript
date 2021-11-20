b3537a50d3d191d6c6c2b1b420f6df71bba1f13f
mdafsha9 <mdafsha9@gmail.com>
2021-11-20 02:41:23 -0600

    for of loop & for in loop

---
+++ forof.js
@@ -0,0 +1,15 @@
+//for of loop & for in loop
+
+//for of loop
+
+let fruits = ["Apple", "Mango", "Pineapple"];
+for(let fruit of fruits){
+  console.log(fruit);
+}
+
+//for in loop, returns index of array
+let vegies = ["Cabbage", "Chilli", "Beans"];
+for(let index in vegies){
+  console.log(index);
+  console.log(vegies[index]);
+}
---
+++ whileInArr.js
@@ -0,0 +1,7 @@
+//while loop in arrays
+let fruits = ["Apple", "Banana", "Mango"];
+let i = 0;
+while(i<fruits.length){
+  console.log(fruits[i]);
+  i++;
+}
