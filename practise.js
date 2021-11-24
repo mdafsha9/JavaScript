108160b8b9a14225a346734c3d3e64946ede2c81
mdafsha9 <mdafsha9@gmail.com>
2021-11-24 03:02:33 -0600

    practising boolean & comparison operator

--- practise.js
+++ practise.js
@@ -1,7 +1,4 @@
-//BigInt
-let num = 212456424324321325;
-console.log(BigInt(num));
-console.log(Number.MAX_SAFE_INTEGER);
-let num1 = BigInt(1323);
-let num2 = 12n;
-console.log(num1+num2);
+//Boolean & comparison operator
+let num1 = 12;
+let num2 = 25;
+console.log(num1<num2);
