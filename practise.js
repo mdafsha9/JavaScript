dd5ebe04eb908351a4dbe9fc46525bd1162e9797
mdafsha9 <mdafsha9@gmail.com>
2021-11-24 00:57:48 -0600

    practising vari,let & const

---
+++ practise.js
@@ -0,0 +1,16 @@
+var firstName = "afsha";
+console.log(firstName);
+var firstName = "Isha";
+console.log(firstName);
+
+//let variables
+
+let lastName = "Wright";
+console.log(lastName);
+lastName ="Krumm";
+console.log(lastName);
+
+//const variables
+const pi = 3.14;
+//pi = 312;
+console.log(pi*2);
