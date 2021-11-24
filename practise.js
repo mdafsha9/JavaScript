2af52d049e378d7be04a982d485a4d97ec8bec8f
mdafsha9 <mdafsha9@gmail.com>
2021-11-24 04:19:00 -0600

    Practising And Or operator

--- practise.js
+++ practise.js
@@ -1,4 +1,9 @@
-//ternary operators
-let age = 4;
- let user = age>=5 ? "coffee":"milk";
-console.log(user);
+//and or operator
+
+let firstName = "loyal";
+let age = 15;
+if(firstName[0] === "R" || age>=18){
+  console.log("FirstName strats with R and 18 years old");
+}else{
+  console.log("age below 18");
+}
