03f389266e4f76031ea15142cd9cdf8aa0a19ea6
mdafsha9 <mdafsha9@gmail.com>
2021-11-24 02:45:39 -0600

    practising typeof and num to strg, strng to num conversion

--- practise.js
+++ practise.js
@@ -1,16 +1,32 @@
-var firstName = "afsha";
-console.log(firstName);
-var firstName = "Isha";
-console.log(firstName);
+//tyeof
 
-//let variables
+let firstName = "Afsha";
+let num = 22;
+console.log(firstName, typeof firstName);
+console.log(num, typeof num);
 
-let lastName = "Wright";
-console.log(lastName);
-lastName ="Krumm";
-console.log(lastName);
+let lastname;
+console.log(typeof lastname);
+lastname = "";
+console.log(typeof lastname);
+lastname= null;
+console.log(lastname, typeof lastname);
+lastname = undefined;
+console.log(typeof lastname, lastname);
 
-//const variables
-const pi = 3.14;
-//pi = 312;
-console.log(pi*2);
+//num to string consversion
+
+var num1 = 22;
+console.log(typeof num1);
+num1 = 22 + " ";
+console.log(typeof num1);
+ var myNum = String(num1);
+ console.log(typeof myNum);
+
+//string to number consversion
+let fruits = "Mango";
+console.log(typeof fruits, fruits);
+let myFruits = +fruits;
+console.log(typeof myFruits);
+let myNewfruits = Number(fruits);
+console.log(typeof myNewfruits);
