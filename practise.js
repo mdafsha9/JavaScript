9fdea8d8c015139addbe1e470c1933b1f7385f30
mdafsha9 <mdafsha9@gmail.com>
2021-11-24 04:59:19 -0600

    about function

--- practise.js
+++ practise.js
@@ -1,13 +1,1 @@
-//nested if else
-let num = prompt("Guess a Number");
-let winningNumber=19;
-if(num === winningNumber){
-  console.log("You win!");
-}else{
-  if(num<winningNumber){
-    console.log("Too low!");
-  }else{
-    console.log("Too high!");
-  }
-}
-console.log(num);
+//
