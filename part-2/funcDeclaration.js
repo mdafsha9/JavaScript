9c6a55fa123b3527b04553cf9673ff09f0aeaafc
mdafsha9 <mdafsha9@gmail.com>
2021-11-26 00:35:16 -0600

    synchronous programming

---
+++ part-2/funcDeclaration.js
@@ -0,0 +1,6 @@
+//function declaration
+//Declared functions are not executed immediately. They can saved for later use, and will be executed later, when they are invoked
+console.log(myFunction(5,3));
+function myFunction(a, b){
+  return a*b;
+}
