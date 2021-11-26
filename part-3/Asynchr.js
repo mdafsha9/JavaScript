e63eaf0c742f30958a823e79718028ec69cd4279
mdafsha9 <mdafsha9@gmail.com>
2021-11-26 00:56:10 -0600

    Asynchronous programmng with setTimeout

---
+++ part-3/Asynchr.js
@@ -0,0 +1,10 @@
+//Asynchronous programming
+
+//setTimeout
+console.log("script start");
+function hello(){
+  console.log("Inside script");
+}
+setTimeout(hello, 1000);
+
+console.log("Script end");
