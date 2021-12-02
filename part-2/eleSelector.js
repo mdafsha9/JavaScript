d9e1a9df4795ffc0500e3bee09549ee1c4b805ef
mdafsha9 <mdafsha9@gmail.com>
2021-12-02 00:42:28 -0600

    getElementById selector

--- part-2/eleSelector.js
+++ part-2/eleSelector.js
@@ -1,17 +1,7 @@
 //select element by using getElementById
 
-const paragraph = document.getElementById("dec");
-console.log(paragraph);
-setTimeout(()=>{
-  paragraph.textContent = "ID Selected";
-  paragraph.style.color = "Violet";
-    console.log(paragraph);
+const para = document.getElementById("dec");
+setTimeout(()=> {
+  para.textConent = "ID SELECTOR"
+  para.style.color = "red";
 }, 1000)
-
-//select element using querySelector
-const heading = document.querySelector(".heading1");
-console.log(heading);
-setTimeout(()=>{
-  heading.textContent = "One"
-  heading.style.color = "blue";
-},2000)
