a4d7edd5987f90f67c073f63f727b03604af2f41
mdafsha9 <mdafsha9.gmail.com>
2021-12-04 05:08:48 -0600

    deleted dom files

--- part-2/eleSelector.js
+++
@@ -1,7 +0,0 @@
-//select element by using getElementById
-
-const para = document.getElementById("dec");
-setTimeout(()=> {
-  para.textConent = "ID SELECTOR"
-  para.style.color = "red";
-}, 1000)
--- part-2/tagSelec.js
+++
@@ -1,7 +0,0 @@
-//finding HTML element by Tag name
-
-const tag = document.getElementsByTagName("h1")[0];
-console.log(tag);
-setTimeout(()=>{
-  tag.style.background = "yellow";
-},2000)
