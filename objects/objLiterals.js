e9180b53117672301925ba2093f797dae681b0aa
mdafsha9 <mdafsha9.gmail.com>
2021-12-06 05:40:49 -0600

    created an object using object literals

--- objects/objLiterals.js
+++ objects/objLiterals.js
@@ -1,1 +1,9 @@
 //create an object using object literals
+const employee = {
+  firstName : "Emmanuel",
+  lastName : "Haul",
+  eyeColor : "Brown",
+  age : 25
+}
+
+console.log("My name is " + employee.firstName + "and I am " + employee.age + " years old");
