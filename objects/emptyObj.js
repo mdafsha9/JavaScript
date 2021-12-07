77ab59805fc1367d22c651914a984e563d7302b8
mdafsha9 <mdafsha9.gmail.com>
2021-12-06 23:20:30 -0600

    create an empty Javascript object

---
+++ objects/emptyObj.js
@@ -0,0 +1,8 @@
+//createa an empty JavaScript objects
+
+const person = {};
+person.firstName = "Mary";
+person.lastName = "Lorie";
+person.age = 30;
+person.studentID = 125012;
+console.log(person.firstName + " " + person.lastName + " is " + person.age + " years old and her student ID is " + person.studentID);
