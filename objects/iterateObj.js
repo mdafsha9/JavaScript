39add0aafe4797f6aa31b7d9e1eeec311e682688
mdafsha9 <mdafsha9.gmail.com>
2021-12-07 02:51:50 -0600

    iterate objects

--- objects/iterateObj.js
+++ objects/iterateObj.js
@@ -4,7 +4,14 @@ const employee = {
   age : 25,
   hobbies : ["cricket", "Reading", "Dancing"]
 }
-for(let key in employee){
-  console.log(key);
+//using for in loop
+// for(let key in employee){
+//   console.log(key);
+//   console.log(employee[key]);
+// }
+
+
+//using Object.keys
+for(let key of Object.keys(employee)){
   console.log(employee[key]);
 }
