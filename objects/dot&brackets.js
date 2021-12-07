dab8d62cc8b5083c847d757b6e8994ebd9ccb061
mdafsha9 <mdafsha9.gmail.com>
2021-12-07 00:02:59 -0600

    diff b/w dot & brackets

---
+++ objects/dot&brackets.js
@@ -0,0 +1,9 @@
+//difference between dot and brackets notation
+
+const person = {
+  name : "Lorie",
+  age : 40,
+  "person hobbies" : ["guitar", "Harmony", "Tabala"]
+}
+console.log(person.name);
+console.log(person["person hobbies"]);
