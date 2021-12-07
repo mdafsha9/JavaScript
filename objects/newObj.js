fa92f414721dbd8a8236e8a8c300d7fd19f27e51
mdafsha9 <mdafsha9.gmail.com>
2021-12-06 23:29:08 -0600

    create an object using new keyword

---
+++ objects/newObj.js
@@ -0,0 +1,8 @@
+//create an object using new keyword
+const person = new Object();
+person.firstName = "Mike";
+person.lastName = "Joy";
+person.age = 25;
+person.eyeColor = "Brown";
+
+console.log("My eye color is " + person.eyeColor);
