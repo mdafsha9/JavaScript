c099d94ebebda4f9c6b6b5c064a97ecb3df76047
mdafsha9 <mdafsha9.gmail.com>
2021-12-10 04:42:36 -0600

    objects inside array

---
+++ objects/ObjInsArr.js
@@ -0,0 +1,11 @@
+//Objects inside array
+//very useful in realworld application
+
+const users = [
+  {userId:1, firstName:"Wright", age:32},
+  {userId:2, firstName:"Lorie", age:25},
+  {userId:3, firstName:"Mercury", age:22}
+]
+  for(let user of users){
+    console.log(user);
+  }
