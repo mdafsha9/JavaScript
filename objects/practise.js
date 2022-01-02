47ed9d3406ed07b04c33ccef5b611bdaac8cc3b7
mdafsha9 <mdafsha9@gmail.com>
2022-01-02 03:47:48 -0600

    slice()

--- objects/practise.js
+++ objects/practise.js
@@ -3,3 +3,8 @@
 let firstName = "MAnOhAr";
 console.log(firstName.toUpperCase());
 console.log(firstName.toLowerCase());
+
+let newString = firstName.slice(0,5);
+console.log(newString);
+newString = firstName.slice(5);
+console.log(newString);
