592203bcd230a3f43514034c33f10cf6023b2091
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 03:50:17 -0600

    and operator

--- objects/practise.js
+++ objects/practise.js
@@ -1,4 +1,11 @@
-//ternary operator
-let age = 2;
-let drink = age>=5 ? "Coffee" : "Milk";
-console.log(drink);
+//And or operator
+
+let firstName = "Hanish";
+let age = 19;
+
+// and operator must give true conditions both otherwise it returns else block
+if(firstName[0]==="H" && age>18){
+  console.log("Your name starts with H and above 18");
+}else{
+  console.log("wrong data");
+}
