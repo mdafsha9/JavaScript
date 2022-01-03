336684e9b1d9205df06e33b50f65128fc08a86d9
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 04:27:43 -0600

    if else if

--- objects/practise.js
+++ objects/practise.js
@@ -1,13 +1,14 @@
-//nested if else
+//if else if
 
-let winningNumber = 19;
-let userGuess = +prompt("Guees a number");
-if(userGuess === winningNumber){
-  console.log("You guess a right number");
+let temp = 22;
+if(temp === 0){
+  console.log("Freezy");
+}else if(temp <15){
+  console.log("Outside cold!")
+} else if(temp<25){
+  console.log("cold!");
+}else if(temp<35){
+  console.log("turn on AC");
 }else{
-  if(userGuess<winningNumber){
-    console.log("Your number guess is low");
-  }else{
-    console.log("your number guess is high");
-  }
+  console.log("Weather is too hot!");
 }
