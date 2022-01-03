80325b834ca0b07fece1e26b8299e84af4d983c3
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 03:02:08 -0600

    == and === difference

--- objects/practise.js
+++ objects/practise.js
@@ -1,5 +1,11 @@
 //Boolean & camparison operator
-let num1 = 23;
+let num1 = "23";
 let num2 = 23;
 
 console.log(num1>=num2);
+
+//== & ===
+//== checks only values
+console.log(num1==num2);
+//=== checks value and typeof also
+console.log(num1===num2);
