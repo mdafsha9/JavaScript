4076a14e0a0540224fa43d4e5bddf19f07818191
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 03:17:16 -0600

    even/odd using if else consition

--- objects/practise.js
+++ objects/practise.js
@@ -1,7 +1,7 @@
 //if else condition
-let age = 19;
-if(age > 18){
-  console.log("User can play Cricket");
+let num = 13;
+if(num%2 === 0){
+  console.log("even");
 }else{
-  console.log("user can play ludo");
+  console.log("odd");
 }
