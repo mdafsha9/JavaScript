5d67d07476d38a0cd3660f34d829d1880846c701
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 03:14:52 -0600

    if else condition

--- objects/practise.js
+++ objects/practise.js
@@ -1,11 +1,7 @@
-//Boolean & camparison operator
-let num1 = "23";
-let num2 = 23;
-
-console.log(num1>=num2);
-
-//== & ===
-//== checks only values
-console.log(num1==num2);
-//=== checks value and typeof also
-console.log(num1===num2);
+//if else condition
+let age = 19;
+if(age > 18){
+  console.log("User can play Cricket");
+}else{
+  console.log("user can play ludo");
+}
