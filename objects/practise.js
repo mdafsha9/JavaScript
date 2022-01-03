c821309b4b4289304a81a261ee5aed6a460e985d
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 04:41:22 -0600

    switch statement

--- objects/practise.js
+++ objects/practise.js
@@ -1,14 +1,28 @@
-//if else if
+//switch statement
 
-let temp = 22;
-if(temp === 0){
-  console.log("Freezy");
-}else if(temp <15){
-  console.log("Outside cold!")
-} else if(temp<25){
-  console.log("cold!");
-}else if(temp<35){
-  console.log("turn on AC");
-}else{
-  console.log("Weather is too hot!");
+let day = 3;
+switch(day){
+  case 0:
+  console.log("Sunday");
+  break;
+  case 1:
+  console.log("Monday");
+  break;
+  case 2:
+  console.log("Tuesday");
+  break;
+  case 3:
+  console.log("Wednesday");
+  break;
+  case 4:
+  console.log("Thursday");
+  break;
+  case 5:
+  console.log("Friday");
+  break;
+  case 6:
+  console.log("Saturday");
+  break;
+  default:
+  console.log("Invalid day");
 }
