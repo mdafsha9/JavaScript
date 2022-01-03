ac95d96b665d1411b73d8239829131488aba0a8b
mdafsha9 <mdafsha9@gmail.com>
2022-01-03 04:10:51 -0600

    nested if else

--- objects/practise.js
+++ objects/practise.js
@@ -1,19 +1,13 @@
-//And or operator
+//nested if else
 
-let firstName = "Hanish";
-let age = 19;
-
-// and operator must give true conditions both otherwise it returns else block
-if(firstName[0]==="H" && age>18){
-  console.log("Your name starts with H and above 18");
+let winningNumber = 19;
+let userGuess = +prompt("Guees a number");
+if(userGuess === winningNumber){
+  console.log("You guess a right number");
 }else{
-  console.log("wrong data");
-}
-
-//or operator either one conition is true
-
-if(firstName[0]==="R" || age>18){
-  console.log("inside if");
-}else{
-  console.log("inside else");
+  if(userGuess<winningNumber){
+    console.log("Your number guess is low");
+  }else{
+    console.log("your number guess is high");
+  }
 }
