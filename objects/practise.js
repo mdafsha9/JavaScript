cf6f82fbc202838ecaf25d405f53c1c8d9c9c257
mdafsha9 <mdafsha9@gmail.com>
2022-01-05 04:07:28 -0600

    push, pop, unshift and shift methods

--- objects/practise.js
+++ objects/practise.js
@@ -1,9 +1,22 @@
-//arrays
-//arrays are reference type
-let fruits = ["Apple", "Mango", "Banana"];
+//pusp, pop arrays & shift ans unshift arrays
+
+
+let fruits = ["Apple", "Mango", "Grapes"];
+console.log(fruits);
+//push mehtod add an element at the end
+fruits.push("Banana");
+console.log(fruits);
+//pop method removes an element at the end
+let poppedFruit = fruits.pop();
+console.log("Popped fruits is " +poppedFruit);
+
 console.log(fruits);
 
-fruits[1]="Grapes";
+//unshift method add an element at the start
+fruits.unshift("Strawberry");
 console.log(fruits);
-console.log(typeof fruits);
-console.log(Array.isArray(fruits));
+
+//shift method removes an element at the start
+let removedFruit = fruits.shift();
+console.log("Removed fruit is " +removedFruit);
+console.log(fruits);
