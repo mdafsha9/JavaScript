246666a752f078cbd2a59194c422303a22a35ba7
mdafsha9 <mdafsha9@gmail.com>
2022-01-05 05:14:11 -0600

    for loop in array

--- objects/practise.js
+++ objects/practise.js
@@ -1,22 +1,6 @@
-//pusp, pop arrays & shift ans unshift arrays
-
-
-let fruits = ["Apple", "Mango", "Grapes"];
+//for loop in arrays
+let fruits = ["Apple", "Mango", "Banana", "Grapes"];
 console.log(fruits);
-//push mehtod add an element at the end
-fruits.push("Banana");
-console.log(fruits);
-//pop method removes an element at the end
-let poppedFruit = fruits.pop();
-console.log("Popped fruits is " +poppedFruit);
-
-console.log(fruits);
-
-//unshift method add an element at the start
-fruits.unshift("Strawberry");
-console.log(fruits);
-
-//shift method removes an element at the start
-let removedFruit = fruits.shift();
-console.log("Removed fruit is " +removedFruit);
-console.log(fruits);
+for(let i=0; i<fruits.length; i++){
+  console.log(fruits[i].toUpperCase());
+}
