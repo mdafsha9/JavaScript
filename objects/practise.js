ae90ad5165cf1da6b58165ab6b7f116e0f2e9841
mdafsha9 <mdafsha9@gmail.com>
2022-01-05 03:50:16 -0600

    Arrays

--- objects/practise.js
+++ objects/practise.js
@@ -1,6 +1,9 @@
-//do while loop
-let i=10; //i =0;
-do{
-  console.log(i);
-  i++
-}while(i<=10)
+//arrays
+//arrays are reference type
+let fruits = ["Apple", "Mango", "Banana"];
+console.log(fruits);
+
+fruits[1]="Grapes";
+console.log(fruits);
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));
