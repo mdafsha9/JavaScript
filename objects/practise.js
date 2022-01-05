7aaf8be4ca8b6d9cbb4bb30ebbb47d899638cadd
mdafsha9 <mdafsha9@gmail.com>
2022-01-05 05:49:26 -0600

    for of loop

--- objects/practise.js
+++ objects/practise.js
@@ -1,7 +1,5 @@
-//while loop in arrays
-let fruits = ["apple", "Mango", "Banana"];
-let i=0;
-while(i<fruits.length){
-  console.log(fruits[i].toUpperCase());
-  i++;
+//for of loop
+let fruits = ["Apple", "Mango", "Grapes", "Banana"];
+for(let fruit of fruits){
+  console.log(fruit);
 }
