66cb40afb4fc910c3b92bb4457c7bfa9213228a4
mdafsha9 <mdafsha9@gmail.com>
2022-01-06 06:59:23 -0600

    iterate objects with for in loop and Object.keys

--- objects/practise.js
+++ objects/practise.js
@@ -6,3 +6,17 @@ let person = {
   "person hobbies" : ["Reading", "Playing", "Singing"]
 }
 console.log(person["person hobbies"]);
+
+// to iterate Objects we use two types
+//1.for in loop
+//2.Object.keys
+
+for(let key in person){
+  console.log(key, ":", person[key]);
+  //Below i am using backtics
+  console.log(`${key} : ${person[key]}`);
+}
+
+for(let key of Object.keys(person)){
+  console.log(person[key]);
+}
