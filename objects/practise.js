37e168833bedce461cdff051573430ea2c09b8b9
mdafsha9 <mdafsha9@gmail.com>
2022-01-06 05:12:27 -0600

    create an object with constructor

--- objects/practise.js
+++ objects/practise.js
@@ -1,6 +1,11 @@
-//create a new keyword
-let person = new Object();
-person.firstName = "Wright";
-person.lastName = "Lorie";
-person.age = 22;
-console.log(person);
+//create an object with constructor
+function Person(first, last, age, eye){
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+let myFather = new Person("Lorie", "Wright", 22, "Brown");
+let myMother = new Person("Rose", "Marie", 25, "Black");
+
+console.log(myFather.firstName);
