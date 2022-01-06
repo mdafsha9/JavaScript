5a41d5da244eed7a18c0e7bc8f577413f12d4256
mdafsha9 <mdafsha9@gmail.com>
2022-01-06 06:39:16 -0600

    diff b/w dot & bracket notation

--- objects/practise.js
+++ objects/practise.js
@@ -1,11 +1,8 @@
-//create an object with constructor
-function Person(first, last, age, eye){
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.eyeColor = eye;
-}
-let myFather = new Person("Lorie", "Wright", 22, "Brown");
-let myMother = new Person("Rose", "Marie", 25, "Black");
+//dot vs bracket notation
 
-console.log(myFather.firstName);
+let person = {
+  firstName : "Gari",
+  age : 22,
+  "person hobbies" : ["Reading", "Playing", "Singing"]
+}
+console.log(person["person hobbies"]);
