e2cdf8d6b7cf8563600ffe104f3b17cd8a3c841b
mdafsha9 <mdafsha9@gmail.com>
2022-01-09 03:53:03 -0600

    objects in arrays

--- objects/practise.js
+++ objects/practise.js
@@ -1,10 +1,10 @@
-//object objDestructuring
-const person = {
-  firstName : "Ale",
-  lastName : "Morie",
-  age : 55,
-  nationality: "Indian"
-}
+//object in arrays
 
-const{fName, lName} = person;
-console.log(fName, lName);
+let myArray = [
+  {userId:1231, firstName:"Lorie", gender:"Female"},
+  {userId:52253, firstName:"Wright", gender:"Male"},
+  {userId:1258, firstName:"Tom", gender:"Others"}
+]
+
+const [{firstName}, , {gender}] = myArray;
+console.log(firstName, gender);
