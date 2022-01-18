ba5cf2a865bb6716eb6cdd54c32913c970ea8061
mdafsha9 <mdafsha9@gmail.com>
2022-01-18 07:44:05 -0600

    some() method with real example

--- methods/some.js
+++ methods/some.js
@@ -5,3 +5,13 @@ let numbers = [3,5,8,9];
 const even = numbers.some((num)=>num%2===0);
 
 console.log(even);
+
+
+let users = [
+  {item1:"Fridge", price:25000},
+  {item1:"Air conditioner", price:15000},
+  {item1:"Television", price:120000},
+]
+
+const Budget = users.some((user)=>user.price>100000);
+console.log(Budget);
