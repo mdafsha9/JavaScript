c6e7ed1a1ba2328fecb07e727c246a44d4c3d33d
mdafsha9 <mdafsha9@gmail.com>
2022-01-18 06:13:52 -0600

    realistic example for sort() method

--- methods/sort.js
+++ methods/sort.js
@@ -18,3 +18,15 @@ const HighToLow = numbers.sort((a,b) => {
   return b-a;
 });
 console.log(HighToLow);
+
+
+let users = [
+  {productId:1, productItem:"Laptop", price:22000},
+  {productId:2, productItem:"Phone", price:45000},
+  {productId:3, productItem:"Tablet", price:80000},
+  {productId:4, productItem:"Strightner", price:2000},
+  {productId:5, productItem:"Dryier", price:1000},
+]
+
+const LowestToHighest = users.sort((a,b)=>a.price-b.price);
+console.log(LowestToHighest);
