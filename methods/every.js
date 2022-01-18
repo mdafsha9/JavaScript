a8a2a085bc67b47905a66229da451d520566ebb5
mdafsha9 <mdafsha9@gmail.com>
2022-01-18 07:21:25 -0600

    every() method with real example

--- methods/every.js
+++ methods/every.js
@@ -3,3 +3,13 @@
 let numbers = [2,4,6,8,10];
   const even = numbers.every((num)=>num%2===0);
   console.log(even);
+
+
+let users = [
+  {item1:"Fridge", price:25000},
+  {item1:"Air conditioner", price:15000},
+  {item1:"Television", price:20000},
+]
+
+const Budget = users.every((user)=>user.price<30000);
+console.log(Budget);
