c5ad1b59945166285c73c2cf0b1cf2c91e3f2e4d
mdafsha9 <mdafsha9@gmail.com>
2022-01-19 10:27:50 -0600

    iterating over a string

---
+++ impTopics/iterables.js
@@ -0,0 +1,9 @@
+//iterables
+//Where we can apply "for of loop" those are iterables. We can apply "for of loop" in strings and arrays
+//strings and arrays are iterables.
+
+//iterating over a string
+let firstName = "Richardson";
+for(let char of firstName){
+  console.log(char);
+}
