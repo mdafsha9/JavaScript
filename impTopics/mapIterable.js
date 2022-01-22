bea935e2495780e1eb7e967fd491fdbc361f206f
mdafsha9 <mdafsha9@gmail.com>
2022-01-22 08:49:19 -0600

    map object

---
+++ impTopics/mapIterable.js
@@ -0,0 +1,28 @@
+//map (map is also a iterable)
+//difference between object and map
+
+//objects can only have string or symbols as a key
+
+const person = {
+  firstName : "Rosie",
+  age : 22,
+  1 : "One"
+}
+console.log(person);
+
+for(let key in person){
+  console.log(key, typeof key);
+}
+
+//in map we can use anything as key like arrays, numbers, strings, objects
+
+const student = new Map();
+student.set('firstName', 'Morie');
+student.set('age', 12);
+student.set(1, 'one');
+student.set([1,2,3], 'oneTwoThree');
+console.log(student);
+console.log(student.get('firstName'));
+for(let key of student.keys()){
+  console.log(key, typeof key);
+}
