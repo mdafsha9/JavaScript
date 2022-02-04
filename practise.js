ea44cfde31e404600aaa3a565b1fd50dfb4ccf53
mdafsha9 <mdafsha9@gmail.com>
2022-02-04 04:18:14 -0600

    string indexing practise

--- practise.js
+++ practise.js
@@ -1,1 +1,18 @@
-//
+let firstName = "MariGold";
+console.log(firstName.length);
+
+console.log(firstName[firstName.length-1]);
+console.log(firstName[1]);
+
+//trim() strmethods
+
+firstName = "      MAriya       ";
+console.log(firstName, firstName.length);
+console.log(firstName.trim(), firstName.length);
+
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+
+let fruitName = "Banana";
+let newString = fruitName.slice(2,4);
+console.log(newString);
