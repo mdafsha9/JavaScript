330766ca067bfe3a259c6c8de33c89f2cf1cadad
mdafsha9 <mdafsha9@gmail.com>
2022-02-04 05:00:41 -0600

    template string

--- practise.js
+++ practise.js
@@ -1,41 +1,6 @@
-//typeof, string to num, num to string conversion
+//template String
 
-let firstName = "Mariya";
-let age = 12;
-let fruits = ["Apple", "Mango", "Grapes"];
-let person = {
-  fName : "Lasya",
-  eyeColor:"Black"
-}
-let lastName;
-console.log(typeof firstName);
-console.log(typeof age);
-console.log(typeof fruits);
-console.log(typeof person);
-console.log(typeof lastName);
+let firstName = "MArie";
+let age =12;
 
-let vegies = null;
-console.log(typeof vegies);
-
-
-let num = 12 + " ";
-console.log(typeof num);
-
-let name = +"Roy";
-console.log(typeof name);
-
-let num1 = 18;
-console.log(typeof num1);
-num2 = String(num1);
-console.log(typeof num2);
-
-let hobby = "Singing";
-console.log(typeof hobby);
-hubby = Number(hobby);
-console.log(typeof hubby);
-
-console.log(Number.MAX_SAFE_INTEGER);
-
-let number = BigInt(123);
-let numb = 12n;
-console.log(number+numb);
+console.log(`${firstName} is ${age} years old.`);
