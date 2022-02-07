c2ef49af5110037db720781470d372448f8085ef
mdafsha9 <mdafsha9@gmail.com>
2022-02-07 08:13:37 -0600

    OR operator either condition is true

--- practise.js
+++ practise.js
@@ -6,8 +6,8 @@
 
 
 let firstName = "Royal";
-let age = 19;
-if( firstName[0] === "R" && age>18){
+let age = 12;
+if( firstName[0] === "R" || age>18){
   console.log("Your name starts with R and age is above 18");
 }else{
   console.log("Sorry! your not qualified.");
