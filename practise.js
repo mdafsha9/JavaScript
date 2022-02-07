6a3f113df928b9bc9591b8759658844b1961c7bd
mdafsha9 <mdafsha9@gmail.com>
2022-02-07 08:00:13 -0600

    falsy values

--- practise.js
+++ practise.js
@@ -1,5 +1,24 @@
-//booleans and camparion
+//if else
 
-let num = 12;
+let num = 18;
 
-console.log(num<15);
+if(num<15){
+  console.log("given num is lessthan 15");
+}else{
+  console.log("given num is greater than 15");
+}
+
+
+
+//false
+//null
+//0
+//undefined
+// " "
+let firstName = "Hoo" ;
+
+if(firstName){
+  console.log(firstName);
+}else{
+  console.log("These are falsy values");
+}
