d3766cc6b5a80ebdb09a7d18781dc4e7ecf02aa8
mdafsha9 <mdafsha9@gmail.com>
2022-02-07 08:11:44 -0600

    And operator both conditions must true

--- practise.js
+++ practise.js
@@ -1,10 +1,14 @@
-//ternary operator
+//and or operator
 
 
 
 
 
-let age = 5;
-let user= age <= 5 ? " milk" : "coffee";
 
-console.log(user);
+let firstName = "Royal";
+let age = 19;
+if( firstName[0] === "R" && age>18){
+  console.log("Your name starts with R and age is above 18");
+}else{
+  console.log("Sorry! your not qualified.");
+}
