009eac96bf3ab416947a0fe944204954fb0ca9d3
mdafsha9 <mdafsha9@gmail.com>
2022-02-23 01:08:30 -0600

    practising function inside function

--- practise.js
+++ practise.js
@@ -1,3 +1,15 @@
-//function expression
-const x = (a,b) => a*b;
-console.log(x(8,2));
+//function inside function
+
+function myFunction(){
+
+  function addTwoNum(a,b){
+    console.log( a+b);
+  }
+  function mulTwoNum(c,d){
+    console.log(c*d);
+  }
+  console.log("Global function");
+  addTwoNum(4,2);
+  mulTwoNum(5,6);
+}
+myFunction();
