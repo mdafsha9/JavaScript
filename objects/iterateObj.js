dc4f53b7197689e85f3fe6e79daa04c3d77f34d2
mdafsha9 <mdafsha9@gmail.com>
2022-04-23 02:03:36 -0500

    iterating over an array

--- objects/iterateObj.js
+++ objects/iterateObj.js
@@ -3,9 +3,16 @@
 //objects are not iterable//iterating over an array or a string//
 //arrays like objects are string and array becuase both are having index and length property
 
-
+//iterating over a string
 let name = "Afsha";
 
 for(let char of name){
   console.log(char);
+}
+
+//iterating over an arrays
+var fruits = ["Apple", "Mango", "Kiwi", "Jack-fruit"];
+
+for(let fruit of fruits){
+  console.log(fruit);
 }
