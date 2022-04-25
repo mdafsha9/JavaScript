622147c02cbcd89895c5c83fd1b69a4877916cd4
mdafsha9 <mdafsha9@gmail.com>
2022-04-25 01:15:55 -0500

    iterating over a string

--- objects/practise.js
+++ objects/practise.js
@@ -1,16 +1,8 @@
 //constructor function
 
 
-function Person(first, last, age) {
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
+let name = "Afsha";
+
+for(let char of name){
+  console.log(char);
 }
-
-const myFather = new Person("John", "Hue", 25);
-const myMother = new Person("Lorie", "Wright", 15);
-console.log(myMother.firstName + ' '+ myMother.lastName + " is a " + myMother.age );
-
-Person.prototype.nationality = "Indian";
-
-console.log("My nationality is " + myMother.nationality);
