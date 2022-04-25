943d834ee619226e3a2b6154374112cd3165f79b
mdafsha9 <mdafsha9@gmail.com>
2022-04-25 02:06:51 -0500

    Set with add property

--- objects/set.js
+++ objects/set.js
@@ -1,9 +1,15 @@
 //JavaScript Set
 
-let numbers = new Set([1,2,3]);
+let numbers = new Set();
+
+
+numbers.add(1);
+numbers.add(4);
+numbers.add(4);
+
+numbers.add(5);
+numbers.add(6);
+numbers.add(7);
 console.log(numbers);
-
-
-
 let char = new Set("abcdefghijklmnopqrstuvwxyz");
 console.log(char);
